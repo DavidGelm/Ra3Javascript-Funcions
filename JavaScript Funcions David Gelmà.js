92142f94d78d962f11d0350491d599f37aef8fed
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-12 19:50:46 +0100

    Exercici 9 Javascript Funcions

--- JavaScript Funcions David Gelmà.js
+++ JavaScript Funcions David Gelmà.js
@@ -1,4 +1,5 @@
-function comprovar(num1,num2){
-    return num1 === 30 || num2 === 30 || (num1+num2) === 30
-}
-console.log(comprovar(20,10))
+const comprovar = function(num1, num2) {
+    return num1 === 30 || num2 === 30 || (num1 + num2) === 30;
+};
+
+console.log(comprovar(20, 10));

15d21214b77cf1eb7580b81311789ec0951aa337
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-12 12:39:29 +0100

    Exercici 5 Javascript Funcions

--- JavaScript Funcions David Gelmà.js
+++ JavaScript Funcions David Gelmà.js
@@ -1,4 +1,12 @@
-function variable_local(){
-    var variable = "patata"
+function Vocals(text) {
+    let contador = 0;
+    const vocals = "aeiouàèéíòóúüAEIOUÀÈÉÍÒÓÚÜ";
+   for(let i=0;i<text.length;i++){
+       if(vocals.includes(text[i])){
+           contador++
+       }
+   }
+   return  "Vocals: " + contador
 }
-console.log(variable)
+
+console.log(Vocals("Patata"));

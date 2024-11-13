5c03044f71f397f0f48d18d1a772eb17627a643d
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-13 14:21:19 +0100

    Exercici 13 Javascript Funcions

--- JavaScript Funcions David Gelmà.js
+++ JavaScript Funcions David Gelmà.js
@@ -1,2 +1,12 @@
-const arrel_quadrada = (String) => String.toLocaleLowerCase()
-console.log(arrel_quadrada("PaTata"))
+const es_null = (valor1,valor2) =>{
+    if(valor1 !==null && valor2 == null ){
+        return valor1
+    }
+    else if(valor2 !==null && valor1==null) {
+        return valor2
+    }
+    else{
+        return null
+    }
+}
+console.log(es_null(1,null))

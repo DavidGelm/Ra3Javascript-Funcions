27e3974eb6e5aff945752ab39dacdbce4a1cd8a4
DavidGelm <david.gelma@institutvidreres.cat>
2024-11-13 17:57:29 +0100

    Exercici 14 Javascript Funcions

--- JavaScript Funcions David Gelmà.js
+++ JavaScript Funcions David Gelmà.js
@@ -1,12 +1,19 @@
-const es_null = (valor1,valor2) =>{
-    if(valor1 !==null && valor2 == null ){
-        return valor1
-    }
-    else if(valor2 !==null && valor1==null) {
-        return valor2
-    }
-    else{
-        return null
-    }
+const posicio = (frase, paraula) => frase.length - frase.indexOf(paraula);
+
+const dividir = (frase, paraula) => frase.slice(frase.indexOf(paraula) + paraula.length).trim();
+
+let frase = "Hola com estàs";
+let paraula = "com";
+
+if (frase.indexOf(paraula) === -1) {
+    console.log("La paraula no es a la frase");
+
+} else {
+    console.log(posicio(frase, paraula));
+    console.log(dividir(frase, paraula));
 }
-console.log(es_null(1,null))
+
+
+
+
+
